863457db5c28e002879ff74b2d4178f7c8665c74
Nadzwa Ray Muhammad <mnadzwaray@gmail.com>
2026-04-18 18:30:01 +0700

    feat:scaffold 12 modul contoller,view,seaders

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,38 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DombaController;
+use App\Http\Controllers\StokPakanController;
+use App\Http\Controllers\ObatVaksinController;
+use App\Http\Controllers\PertumbuhanController;
+use App\Http\Controllers\KesehatanController;
+use App\Http\Controllers\PakanIndividualController;
+use App\Http\Controllers\ReproduksiController;
+use App\Http\Controllers\SilsilahController;
+use App\Http\Controllers\DailyTaskController;
+use App\Http\Controllers\NotifikasiController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('domba', DombaController::class);
+    Route::resource('stok-pakan', StokPakanController::class);
+    Route::resource('obat-vaksin', ObatVaksinController::class);
+    Route::resource('pertumbuhan', PertumbuhanController::class);
+    Route::resource('kesehatan', KesehatanController::class);
+    Route::resource('pakan-individual', PakanIndividualController::class);
+    Route::resource('reproduksi', ReproduksiController::class);
+    Route::resource('silsilah', SilsilahController::class);
+    Route::resource('daily-task', DailyTaskController::class);
+    Route::resource('notifikasi', NotifikasiController::class);
+    Route::resource('users', UserController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
